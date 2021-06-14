1df4ff9cd17926552534c2dd6f182a94d746d50a
michaeliyke <ymichaelc@gmail.com>
2021-06-14 07:54:07 +0100

    Dop down with JavaScript & css: DEMO 1
    Drop down with PURE CSS: DEMO 2

---
+++ t/t.js
@@ -0,0 +1,38 @@
+(function(d) {
+	const query = d.querySelector.bind(d);
+	
+	const caret = query(".menu-bar ul .fa-caret-down");
+	const dropdown = query(".menu-bar ul li .drop-down-menu");
+	const heroImage = query("section.hero");
+
+	function toggleClass(element, name, eventTarget) {
+		if (eventTarget != element) {
+			element.classList.remove(name);
+			return
+		}
+		if (element.classList.contains(name)) {
+			element.classList.remove(name);
+		} else {
+			element.classList.add(name);
+		}
+	}
+
+	d.addEventListener("click", (e) => {
+		const target = e.target;
+		switch(true) {
+			case target == caret:
+				toggleClass(dropdown, "show-block", dropdown);
+				break
+			case target == heroImage:
+				toggleClass(dropdown, "show-block");
+				break
+		}
+	});
+
+	window.addEventListener("keydown", (e) => {
+		if(e.keyCode == 27) {
+			toggleClass(dropdown, "show-block");
+		}
+	});
+
+}(document));
