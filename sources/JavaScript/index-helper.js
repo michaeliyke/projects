ab5190cae3fcd570a5cc83c912b1f43a5c0c1f66
michaeliyke <ymichaelc@gmail.com>
2021-08-15 23:02:56 +0100

    route indicator

--- sources/JavaScript/index-helper.js
+++ sources/JavaScript/index-helper.js
@@ -1,29 +1,47 @@
-Techie(function($, body, head, d, _, w, Log, stringify, stringifyAll, a){
-  
+/* jshint -W107 */
+Techie(function ($, body, head, d, _, w, Log, stringify, stringifyAll, a) {
+const log = console.log;
 
 
-// getcss("display")
+  // getcss("display")
 
 
 
 
-function log_prop_value(event, prop) {
-  x = $(event.target).getStyle(prop) | console.log(x);
-  return x;
-}
+  function log_prop_value(event, prop) {
+    x = $(event.target).getStyle(prop) | console.log(x);
+    return x;
+  }
 
-function getcss(css) {
-  
-  $(w).resize( function(event){
-  console.log(w.innerWidth)
-});
+  function getcss(css) {
 
-  $(body).click(function(event) {
-    a(w.innerWidth)
-    Log(w.innerWidth)
-    console.log($(event.target).getStyle(css))
+    $(w).resize(function (event) {
+      console.log(w.innerWidth);
+    });
+
+    $(body).click(function (event) {
+      a(w.innerWidth);
+      Log(w.innerWidth);
+      console.log($(event.target).getStyle(css));
+    });
+  }
+  function getRoute() {
+    const l = window.location.href;
+    const x = l.split("/").reverse();
+    let route = "";
+   if (x[0]) {
+     route = x[0];
+   } else if (x[1]) {
+     route = x[1];
+   }
+   return route;
+  }
+const route = getRoute();
+  $("#main").Attribute("route", body).each((element, index) => {
+    if (element.getAttribute("route") == route) {
+      element.classList.add("current-page");
+      element.setAttribute("href", "JavaScript:void(0)");
+      return {};
+    }
   });
-}
-
-
 });
